4a8ccb7ecdd90df1320ffd87fa10d058dc4b4bc6
jinyoung02 <jink8962@naver.com>
2025-08-09 15:01:53 +0900

    init

---
+++ src/main/java/com/springboot/gabombackend/GabomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.gabombackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabomBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabomBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/gabombackend/GabomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.gabombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
