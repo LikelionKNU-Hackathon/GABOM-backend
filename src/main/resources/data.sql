b87c6c61813f795c24a95b103d4fc907162614f5
jinyoung02 <jink8962@naver.com>
2025-08-12 14:28:21 +0900

    스탬프 테이블 및 초기 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,40 @@
+INSERT INTO stamps (name, category, image_url) VALUES
+                                                   ('asia_1', 'asia', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/asia_1.png'),
+                                                   ('asia_2', 'asia', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/asia_2.png'),
+                                                   ('asia_3', 'asia', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/asia_3.png'),
+
+                                                   ('bunsik_1', 'bunsik', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/bunsik_1.png'),
+                                                   ('bunsik_2', 'bunsik', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/bunsik_2.png'),
+                                                   ('bunsik_3', 'bunsik', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/bunsik_3.png'),
+
+                                                   ('cafe_1', 'cafe', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/cafe_1.png'),
+                                                   ('cafe_2', 'cafe', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/cafe_2.png'),
+                                                   ('cafe_3', 'cafe', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/cafe_3.png'),
+
+                                                   ('china_1', 'china', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/china_1.png'),
+                                                   ('china_2', 'china', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/china_2.png'),
+                                                   ('china_3', 'china', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/china_3.png'),
+
+                                                   ('game_1', 'game', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/game_1.png'),
+                                                   ('game_2', 'game', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/game_2.png'),
+                                                   ('game_3', 'game', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/game_3.png'),
+
+                                                   ('japan_1', 'japan', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/japan_1.png'),
+                                                   ('japan_2', 'japan', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/japan_2.png'),
+                                                   ('japan_3', 'japan', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/japan_3.png'),
+
+                                                   ('korea_1', 'korea', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/korea_1.png'),
+                                                   ('korea_2', 'korea', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/korea_2.png'),
+                                                   ('korea_3', 'korea', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/korea_3.png'),
+
+                                                   ('pub_1', 'pub', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/pub_1.png'),
+                                                   ('pub_2', 'pub', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/pub_2.png'),
+                                                   ('pub_3', 'pub', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/pub_3.png'),
+
+                                                   ('service_1', 'service', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/service_1.png'),
+                                                   ('service_2', 'service', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/service_2.png'),
+                                                   ('service_3', 'service', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/service_3.png'),
+
+                                                   ('western_1', 'western', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/western_1.png'),
+                                                   ('western_2', 'western', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/western_2.png'),
+                                                   ('western_3', 'western', 'https://gabom-stamps.s3.ap-northeast-2.amazonaws.com/stamps/western_3.png');
