43720f2c57166d98a7b8d499c2cb97cd4c2c381b
jinyoung02 <jink8962@naver.com>
2025-08-14 18:51:23 +0900

    유저 스탬프 기반 티어 계산 기능 추가

---
+++ src/main/java/com/springboot/gabombackend/controller/TierController.java
@@ -0,0 +1,26 @@
+package com.springboot.gabombackend.controller;
+
+import com.springboot.gabombackend.dto.TierResponse;
+import com.springboot.gabombackend.service.TierService;
+import com.springboot.gabombackend.entity.User;
+import com.springboot.gabombackend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users/me/tiers")
+public class TierController {
+
+    private final TierService tierService;
+    private final UserService userService;
+
+    @GetMapping
+    public TierResponse getMyTiers(Authentication authentication) {
+        Long userId = (Long) authentication.getPrincipal();
+        User user = userService.getById(userId); // DB 조회
+        return tierService.getUserTiers(user);
+    }
+}
