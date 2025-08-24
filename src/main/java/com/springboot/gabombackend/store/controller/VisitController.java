f9f7f8c0cba307cc226ccf20cc6b4c4e619f6d9d
jinyoung02 <jink8962@naver.com>
2025-08-24 18:14:37 +0900

    수정

--- src/main/java/com/springboot/gabombackend/store/controller/VisitController.java
+++ src/main/java/com/springboot/gabombackend/store/controller/VisitController.java
@@ -1,6 +1,8 @@
 package com.springboot.gabombackend.store.controller;
 
 import com.springboot.gabombackend.store.service.VisitService;
+import com.springboot.gabombackend.user.entity.User;
+import com.springboot.gabombackend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +15,16 @@ import java.security.Principal;
 public class VisitController {
 
     private final VisitService visitService;
+    private final UserRepository userRepository;
 
     // QR 인증 + 스탬프 적립
     @PostMapping("/verify")
     public ResponseEntity<?> verifyVisit(@RequestParam Long storeId, Principal principal) {
-        Long userId = Long.valueOf(principal.getName()); // JWT에서 유저 ID 꺼내오기
-        String message = visitService.verifyVisitAndAddStamp(userId, storeId);
+        String loginId = principal.getName();
+        User currentUser = userRepository.findByLoginId(loginId)
+                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
+
+        String message = visitService.verifyVisitAndAddStamp(currentUser.getId(), storeId);
         return ResponseEntity.ok().body(message);
     }
 }
